31cc067ad9579931c0a92baa389a71f170c1c1ee
itssakinahh8971 <108936825+itssakinahh8971@users.noreply.github.com>
2022-07-13 20:37:18 +0200

    Update server.py

--- server.py
+++ server.py
@@ -50,25 +50,13 @@ def unknown_text(update: Update, context: CallbackContext):
     update.message.reply_text(
         "Sorry I can't recognize you , you said '%s'" % update.message.text)
 
-def s3cr3tcmd(update: Update, context):
-    list2 = ["where is the secret ?","hope you are here with the secret ..." ,"Enter the secret please","Where is my secret ?","now it is the moment you are waiting for , But where is the secret ?"]
-    if len(context.args) == 0 :
-        update.message.reply_text(random.choice(list2))
-        return
-    secret = ''.join(context.args)
-    if secret == "REDACTED" :
-        update.message.reply_text("You have proved you are well determined and for that you can get your prize now :  flag{REDACTED}")
-    else :
-        update.message.reply_text("BAD SECRET ... NO PRIZES FOR YOU !")
-
-
+    
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('name', name,pass_args=True))
 updater.dispatcher.add_handler(CommandHandler('help', help))
 updater.dispatcher.add_handler(CommandHandler('bye', bye))
 updater.dispatcher.add_handler(CommandHandler('missed', missed))
-updater.dispatcher.add_handler(CommandHandler('s3cr3tcmd', s3cr3tcmd,pass_args=True))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
 updater.dispatcher.add_handler(MessageHandler(
     Filters.command, unknown)) # Filters out unknown commands
